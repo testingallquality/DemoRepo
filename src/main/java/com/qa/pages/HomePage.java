96fd4d636ea9a7410b0a31a2fd2f982e3035ba40
testingallquality <44398662+testingallquality@users.noreply.github.com>
2018-10-24 06:14:53 -0400

    first git commit for my selenium code

---
+++ src/main/java/com/qa/pages/HomePage.java
@@ -0,0 +1,9 @@
+package com.qa.pages;
+
+public class HomePage {
+
+    public  static void homePage(){
+        System.out.println("home page");
+    }
+
+}
---
+++ src/test/java/com/qa/tests/LoginTest.java
@@ -0,0 +1,12 @@
+package com.qa.tests;
+
+
+public class LoginTest {
+
+    @test
+    public void test1(){
+
+        System.out.println("test1");
+    }
+
+}
